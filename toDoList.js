6e6faeb3029db8a9a713111963fcf895f87ed63b
masoumeh Salimi <salimi.ma@gmail.com>
2024-03-27 17:07:31 +0330

    add edit option

--- toDoList.js
+++ toDoList.js
@@ -7,6 +7,7 @@ const todoForm = document.querySelector(".todo-form");
 const todoInput = document.querySelector(".todo-input");
 const todoList = document.querySelector(".todolist");
 const filteredTodos = document.querySelector(".filter-todos");
+const bodyy = document.querySelector("#body");
 
 //event:
 
@@ -75,8 +76,11 @@ function createTodo(todos) {
       ).toLocaleDateString("fa-IR")}</span>
       <button class="todo__check" data-todo-id=${todo.id}><i class="far ${
       todo.isCompleted ? "fa-regular fa-square" : "fa-check-square"
-    }"></i></button>
-      <span><i class="far fa-edit"></i></span>
+    }">
+      </i></button>
+      <button class="todo__edit" data-todo-id=${
+        todo.id
+      }><i class="far fa-edit"></i></button>
       <button class="todo__remove" data-todo-id=${
         todo.id
       }><i class="far fa-trash-alt"></i></button>
@@ -89,6 +93,9 @@ function createTodo(todos) {
 
   const checkBtns = [...document.querySelectorAll(".todo__check")];
   checkBtns.forEach((btn) => btn.addEventListener("click", checkTodo));
+
+  const editBtns = [...document.querySelectorAll(".todo__edit")];
+  editBtns.forEach((btn) => btn.addEventListener("click", editTodo));
 }
 
 function removeTodo(e) {
@@ -113,12 +120,72 @@ function checkTodo(e) {
   filterTodos();
 }
 
+function editTodo(e) {
+  const todos = getAllTodos();
+  const todoId = Number(e.target.dataset.todoId);
+  const todo = todos.find((todo) => todo.id == todoId);
+  createModal(todo);
+}
+
+function createModal(todoInput) {
+  let result = "";
+  const backdropDiv = document.createElement("div");
+  backdropDiv.classList.add("backdrop");
+  result = `
+       <div class="modal">
+         <div class="modal__header">
+           <p>ویرایش اطلاعات</p>
+           <button class="close-modal">❌</button>
+         </div>
+         <div class="modal__content">   
+           <form class="form">
+             <div class="form__control">
+               <label for="name">شرح فعالیت</label>
+               <input type="text" name="description" id="description" value=${todoInput.title}>
+             </div>             
+           </form>   
+         </div>
+         <div class="modal__actions">
+           <button class="btn btn--primary">تایید</button>           
+         </div>
+       </div>
+      `;
+
+  document.body.appendChild(backdropDiv);
+  backdropDiv.innerHTML = result;
+
+  backdrop = document.querySelector(".backdrop");
+  const closeModalBtn = document.querySelector(".close-modal");
+  closeModalBtn.addEventListener("click", (e) => {
+    backdrop.classList.add("hidden");
+    document.body.removeChild(backdropDiv);
+  });
+
+  const confirmBtn = document.querySelector(".btn--primary");
+  
+  confirmBtn.addEventListener("click", (e) => {
+    todoInput.title = document.querySelector("#description").value.trim();
+    updateTodo(todoInput);
+    backdrop.classList.add("hidden");
+    document.body.removeChild(backdropDiv);
+    const todos = getAllTodos();
+    saveAllTodos(todos);
+    filterTodos();
+  });
+}
+
 function getAllTodos() {
   const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
   //console.log(savedTodos);
   return savedTodos;
 }
 
+function updateTodo(todo) {
+  const savedTodos = getAllTodos().filter((t) => t.id != todo.id);
+  savedTodos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(savedTodos));
+  return savedTodos;
+}
 function saveTodo(todo) {
   const savedTodos = getAllTodos();
   savedTodos.push(todo);
